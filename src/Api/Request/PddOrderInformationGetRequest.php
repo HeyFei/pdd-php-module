ae7e6d226fad7e800c6bfed5ee89749901df1dc9
laosha <laosha@dreamnavi.cn>
2024-01-15 10:29:29 +0800

    pdd sdk init

---
+++ src/Api/Request/PddOrderInformationGetRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace LZPdd\Api\Request;
+
+use LZPdd\PopBaseHttpRequest;
+use LZPdd\PopBaseJsonEntity;
+
+class PddOrderInformationGetRequest extends PopBaseHttpRequest
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @JsonProperty(String, "order_sn")
+     */
+    private $orderSn;
+
+    protected function setUserParams(&$params)
+    {
+        $this->setUserParam($params, "order_sn", $this->orderSn);
+
+    }
+
+    public function getVersion()
+    {
+        return "V1";
+    }
+
+    public function getDataType()
+    {
+        return "JSON";
+    }
+
+    public function getType()
+    {
+        return "pdd.order.information.get";
+    }
+
+    public function setOrderSn($orderSn)
+    {
+        $this->orderSn = $orderSn;
+    }
+}
---
+++ src/PopHttpException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LZPdd;
+
+/**
+ * Pop接口调用的异常类
+ */
+class PopHttpException extends \Exception
+{
+    public function errorMessage()
+    {
+        return $this->getMessage();
+    }
+}
